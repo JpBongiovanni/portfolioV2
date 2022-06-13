4c0b661b121d536da3a07b5dc94ea59595ee2c29
Jason Bongiovanni <jpbon@protonmail.com>
2022-06-13 14:43:54 -0400

    fixing email function

--- portfolioV2/settings.py
+++ portfolioV2/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jpbongiovanni@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '') or config('Email_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 Email_USE_SSL = False
 
